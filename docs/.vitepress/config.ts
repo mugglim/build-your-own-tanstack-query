30d53d53e5606c0e31f66ec2f4b2a0564cc20ea2
Jaeuk Lim(임재욱) <applewodnr@naver.com>
2025-03-15 22:23:26 +0900

    chore: 검색 기능 추가

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   },
   themeConfig: {
     siteTitle: "Home",
+    search: {
+      provider: "local"
+    },
     nav: [{ text: "학습하기", link: "/intro" }],
     sidebar: [
       {
