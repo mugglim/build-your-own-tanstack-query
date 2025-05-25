18ff46d010ddbdb27ce28afb22bf7d52103deca5
SalmanGits <belalahmad10310@gmail.com>
2025-05-26 01:23:01 +0530

    chore:readability

--- tanstack-query-lite/react/ReactQueryDevtools.jsx
+++ tanstack-query-lite/react/ReactQueryDevtools.jsx
@@ -12,6 +12,14 @@ const ReactQueryDevtools = () => {
 
   const queries = queryClient.getQueryCache().getAll();
   const sortedQueries = [...queries].sort((a, b) => (a.queryHash > b.queryHash ? 1 : -1));
+  function renderStatus(status, isFetching, observers) {
+    if (isFetching) return <span className="text-blue-500">fetching</span>;
+    if (!observers.length) return <span className="text-gray-500">inactive</span>;
+    if (status === "success") return <span className="text-green-500">success</span>;
+    if (status === "error") return <span className="text-red-500">error</span>;
+    return null;
+  }
+
 
   return (
     <div className="fixed bottom-0 w-full overflow-scroll text-white bg-black divide-y-2 divide-gray-800 divide-solid">
@@ -24,27 +32,7 @@ const ReactQueryDevtools = () => {
         return (
           <div key={queryHash} className="p-2">
             {JSON.stringify(queryKey, null, 2)}, {JSON.stringify({ staleTime, gcTime }, null, 2)} -{" "}
-            <span className="font-bold">
-              {(() => {
-                if (isFetching) {
-                  return <span className="text-blue-500">fetching</span>;
-                }
-
-                if (!observers.length) {
-                  return <span className="text-gray-500">inactive</span>;
-                }
-
-                if (status === "success") {
-                  return <span className="text-green-500">success</span>;
-                }
-
-                if (status === "error") {
-                  return <span className="text-red-500">error</span>;
-                }
-
-                return null;
-              })()}
-            </span>
+            <span className="font-bold">{renderStatus(status, isFetching, observers)}</span>
           </div>
         );
       })}
